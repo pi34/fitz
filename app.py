579663a9c779ed0273bf8fd148e0925ee9f4023d
Riya Manchanda <59563727+pi34@users.noreply.github.com>
2024-11-09 19:31:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -25,13 +25,14 @@ def quiz():
 def handle_answer(data):
     correct = data.get('correct', False)
     # Adjust score based on quiz answer
+    points = 0
     if correct:
-        game_state['score'] += 10  # Increase score for correct answer
+        points += 20  # Increase score for correct answer
     else:
-        game_state['score'] -= 5   # Decrease score for incorrect answer
+        points -= 10   # Decrease score for incorrect answer
 
     # Broadcast the updated score to all clients on the game page
-    emit('update_score', {'score': game_state['score']}, broadcast=True)
+    emit('update_score', {'score': points}, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app, port=8000, host='172.20.10.2', debug=True)
